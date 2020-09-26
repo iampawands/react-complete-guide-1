98003119ed73cfe3c09ced07af39a617f30ffaec
iampawands <ubuntu@ip-172-31-5-252.ec2.internal>
2020-09-26 11:54:12 +0000

    Changed class to function.Used useState hook for function which alternative for state in class Componenet

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Person from './Person/Person.js';
-class App extends Component {
-  state = {
+
+
+const app = () =>{
+   const [personState,setPersonState] = useState({
     persons : [{name:'Pawan',age:18},
     {name:'Amsn',age:18},
     {name:'Jaye',age:18}
     ]
-  }
+  });
   
-  changeState = () => {
-    this.setState({
+  const changeState = () => {
+    setPersonState({
     persons : [{name:'Pawanaa',age:44},
     {name:'Amsnaa',age:33},
     {name:'Jayeaa',age:22}
     ]
   });
   }
-  render() {
     return (
       <div className="App">
         <header className="App-header">
@@ -28,15 +29,14 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>#include world.h</code> and save to reload.
         </p>
-        <Person name={this.state.persons[0].name} Age={this.state.persons[0].age}/>
-        <Person name={this.state.persons[1].name} Age={this.state.persons[1].age}>Inside component tags</Person>
-        <Person name={this.state.persons[2].name} Age={this.state.persons[2].age}/>
+        <Person name={personState.persons[0].name} Age={personState.persons[0].age}/>
+        <Person name={personState.persons[1].name} Age={personState.persons[1].age}>Inside component tags</Person>
+        <Person name={personState.persons[2].name} Age={personState.persons[2].age}/>
         
-        <button onClick={this.changeState}>Change State</button>
+        <button onClick={changeState}>Change State</button>
       </div>
     );
-  }
 }
 
-export default App;
+export default app;
 
