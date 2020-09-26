bbebc4a07c852bdbc04626d753b23a1f2f41bf3e
Ubuntu <ubuntu@ip-172-31-5-252.ec2.internal>
2020-09-26 10:43:37 +0000

    First commit react-complete-guid

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Person from './Person/Person.js'
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>#include world.h</code> and save to reload.
+        </p>
+        <Person name='Pawan' Age='18'/>
+        <Person name='Amsn' Age='19'/>
+        <Person name='Jaye' Age='20'/>
+        
+      </div>
+    );
+  }
+}
+
+export default App;
+
