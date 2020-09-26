63f9eb60d8ff9b5b287c1b2f5447b1f46c3dfcf1
iampawands <ubuntu@ip-172-31-5-252.ec2.internal>
2020-09-26 13:45:24 +0000

    Two way binding using input box

--- src/App.js
+++ src/App.js
@@ -18,6 +18,17 @@ class App extends Component {
     ]
   });
   }
+  
+  changeTextHandler = (event) =>{
+      this.setState({
+    persons : [
+    {name:"Pawan",age:44},
+    {name:event.target.value,age:33},
+    {name:'Jayeaa',age:22}
+    ]
+  });
+  }
+  
   render() {
     return (
       <div className="App">
@@ -25,11 +36,13 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>#include world.h</code> and save to reload.
-        </p>
         <Person name={this.state.persons[0].name} Age={this.state.persons[0].age}/>
-        <Person name={this.state.persons[1].name} Age={this.state.persons[1].age} click={this.changeState.bind(this,"abc")}>Inside component tags</Person>
+        <Person 
+        name={this.state.persons[1].name} 
+        Age={this.state.persons[1].age} 
+        click={this.changeState.bind(this,"abc")}
+        textChange={this.changeTextHandler}
+        >Inside component tags</Person>
         <Person name={this.state.persons[2].name} Age={this.state.persons[2].age}/>
         
         <button onClick={this.changeState.bind(this,"xyz")}>Change State</button>
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -4,6 +4,7 @@ const person = (props) => {
     return (
         <div>
         <p onClick={props.click}>{props.name} is {props.Age} years old. {props.children}</p>
+        <input type="text" onChange={props.textChange} />
         </div>    
     );
 }
