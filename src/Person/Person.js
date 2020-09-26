8c556dcfbf0f9b8f6a318e75671f8ba0afda66c8
iampawands <ubuntu@ip-172-31-5-252.ec2.internal>
2020-09-26 13:54:57 +0000

    Two way binding completed

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -4,7 +4,7 @@ const person = (props) => {
     return (
         <div>
         <p onClick={props.click}>{props.name} is {props.Age} years old. {props.children}</p>
-        <input type="text" onChange={props.textChange} />
+        <input type="text" onChange={props.textChange} value={props.name}/>
         </div>    
     );
 }
