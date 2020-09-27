d6648cd546c98e1a24b50f1198b60ed57ba376c8
iampawands <ubuntu@ip-172-31-5-252.ec2.internal>
2020-09-27 09:18:16 +0000

    Added StyledDiv in Perons Compoment

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import Radium from 'radium'
+import styled from 'styled-components'
 import './Person.css'
 const person = (props) => {
-    const style={
-        '@media (min-width:500px)' : {
-            width : '450px'
-        }
-    }
+    const StyledDiv = styled.div`
+        width: 50%;
+        padding: 12px 20px;
+        margin: 10px auto;
+        box-sizing: border-box;
+        border: 1px solid black;
+        text-align: center;
+        @media (min-width:500px) : {
+        width : '450px'
+        }`;
+    
     return (
-        <div className='Person' style={style}>
+        <StyledDiv>
         <p onClick={props.click}>{props.name} is {props.Age} years old. {props.children}</p>
         <input type="text" onChange={props.textChange} value={props.name}/>
-        </div>    
+        </StyledDiv>    
     );
 }
 
