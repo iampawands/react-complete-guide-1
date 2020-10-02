d36ace00b8cbd0647af39c3c1f71aa22eb85f9b4
iampawands <iam.pawands@gmail.com>
2020-10-02 19:11:02 +0530

    Adjacent Jsx Elements using return [] provided key for each element.

--- src/Components/Persons/Person/Person.js
+++ src/Components/Persons/Person/Person.js
@@ -4,12 +4,11 @@ import classes from './Person.css'
 class Person extends Component{
     render(){
          console.log('Person rendered..')
-    return (
-        <div className={classes.Person}>
-        <p onClick={this.props.click}>{this.props.name} is {this.props.Age} years old. {this.props.children}</p>
-        <input type="text" onChange={this.props.textChange} value={this.props.name}/>
-        </div>    
-    );
+    return [
+        <p key='112' onClick={this.props.click}>{this.props.name} is {this.props.Age} years old. {this.props.children}</p>,
+        <input key='1223' type="text" onChange={this.props.textChange} value={this.props.name}/>
+
+    ];
     }
 }
 
