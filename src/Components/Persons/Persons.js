d3a914e24156313288d90754a82f669b6e0eff07
iampawands <iam.pawands@gmail.com>
2020-10-02 19:02:52 +0530

    Instead of checking  all props in shouldComponentUpdate use extends PureComponent It by default check everything. no need of shouldCompnentUpdate

--- src/Components/Persons/Persons.js
+++ src/Components/Persons/Persons.js
@@ -1,16 +1,20 @@
-import React ,{Component}from 'react'
+import React ,{PureComponent}from 'react'
 import Person from './Person/Person';
 
-class Persons extends Component{
+class Persons extends PureComponent{
     // static getDerivedStateFromProps(state,props){
     //     console.log(' Persons [getDerivedStateFromProps]',props);
     //     return state;
     // }
 
-    shouldComponentUpdate(nextProps, nextState, nextContext) {
-        console.log('Persons [shouldComponentUpdate]')
-        return !this.props.persons !== nextProps.persons
-    }
+    //
+    // shouldComponentUpdate(nextProps, nextState, nextContext) {
+    //     console.log('Persons [shouldComponentUpdate]')
+    //     if(nextProps.clicked !== this.props.clicked || this.props.changed !== nextProps.changed || this.props.personList !== nextProps.personList){
+    //         return true;
+    //     }
+    //     return false;
+    // }
 
     getSnapshotBeforeUpdate(prevProps, prevState) {
         console.log('Persons [getSnapshotBeforeUpdate]')
