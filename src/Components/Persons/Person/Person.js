2991530d8fc6eafe24ee53dd643c7c54c2061ffb
iampawands <iam.pawands@gmail.com>
2020-10-02 19:28:33 +0530

    2nd approach : Using HOC high order component. Which basically return the props.children

--- src/Components/Persons/Person/Person.js
+++ src/Components/Persons/Person/Person.js
@@ -1,14 +1,16 @@
 import React, {Component} from 'react';
 import classes from './Person.css'
+import Auxx from '../../../hoc/Auxilary'
 
 class Person extends Component{
     render(){
          console.log('Person rendered..')
-    return [
-        <p key='112' onClick={this.props.click}>{this.props.name} is {this.props.Age} years old. {this.props.children}</p>,
-        <input key='1223' type="text" onChange={this.props.textChange} value={this.props.name}/>
-
-    ];
+    return (
+            <Auxx>
+                <p  onClick={this.props.click}>{this.props.name} is {this.props.Age} years old. {this.props.children}</p>
+                <input  type="text" onChange={this.props.textChange} value={this.props.name}/>
+            </Auxx>
+        );
     }
 }
 
---
+++ src/hoc/Auxilary.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const auxx = props => props.children;
+
+export  default  auxx;
