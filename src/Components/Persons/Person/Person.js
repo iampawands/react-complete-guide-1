5f4ec86dbbe47eb37760dc96751bb343449d20a4
iampawands <iam.pawands@gmail.com>
2020-10-02 19:53:04 +0530

    passing props to Wrapper componet using spread operator. i.e. Passing unknown props.

--- src/Components/Persons/Person/Person.js
+++ src/Components/Persons/Person/Person.js
@@ -1,18 +1,19 @@
 import React, {Component} from 'react';
 import classes from './Person.css'
 import Auxx from '../../../hoc/Auxilary'
+import withClass from "../../../hoc/withClass";
 
 class Person extends Component{
     render(){
          console.log('Person rendered..')
     return (
-            <React.Fragment>
+            <Auxx>
                 <p  onClick={this.props.click}>{this.props.name} is {this.props.Age} years old. {this.props.children}</p>
                 <input  type="text" onChange={this.props.textChange} value={this.props.name}/>
-            </React.Fragment>
+            </Auxx>
         );
     }
 }
 
 
-export default Person;
+export default withClass(Person,classes.Person);
