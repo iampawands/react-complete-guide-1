e383b7f52386bf69b7bb0454d1b4bb58134b3b51
iampawands <iam.pawands@gmail.com>
2020-10-02 19:30:30 +0530

    3rd Approach Using React.Fragment. Under the hood it is same as Aux we created in the previous commit.

--- src/Components/Persons/Person/Person.js
+++ src/Components/Persons/Person/Person.js
@@ -6,10 +6,10 @@ class Person extends Component{
     render(){
          console.log('Person rendered..')
     return (
-            <Auxx>
+            <React.Fragment>
                 <p  onClick={this.props.click}>{this.props.name} is {this.props.Age} years old. {this.props.children}</p>
                 <input  type="text" onChange={this.props.textChange} value={this.props.name}/>
-            </Auxx>
+            </React.Fragment>
         );
     }
 }
